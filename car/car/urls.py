90632c5632916c6feeeeaf5a10a37fbf79523e68
Wivan18 <wawireivan18@gmsil.com>
2023-09-11 15:38:58 +0300

    second commit

--- car/car/urls.py
+++ car/car/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls'))
+    path('home',views.home, name='home'),
+    path('buy',views.buy, name='buy'),
+    path('sell',views.sell, name='sell'),
+    path('',views.index, name='index')
 ]
--- car/home/views.py
+++ car/home/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 def index(request):
     return render(request, 'base.html')
+
+def home(request):
+    return render(request, 'home.html')
+def buy(request):
+    return render(request, 'buy.html')
+def sell(request):
+    return render(request, 'sell.html')
