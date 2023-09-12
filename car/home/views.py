2e8e7e1709835b18e756e63d56cc537a73153da9
Wivan18 <wawireivan18@gmsil.com>
2023-09-12 12:07:04 +0300

    Fourth commit

--- car/home/views.py
+++ car/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from sell.models import category,car
 
 def index(request):
     return render(request, 'base.html')
@@ -7,5 +8,6 @@ def home(request):
     return render(request, 'home.html')
 def buy(request):
     return render(request, 'buy.html')
+    
 def sell(request):
     return render(request, 'sell.html')
